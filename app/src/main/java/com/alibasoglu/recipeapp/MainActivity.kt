8d397db64a2636a2a5d4159a0962d7b344547b6f
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-06-17 19:57:37 +0300

    fix

--- app/src/main/java/com/alibasoglu/recipeapp/MainActivity.kt
+++ app/src/main/java/com/alibasoglu/recipeapp/MainActivity.kt
@@ -29,10 +29,6 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                    // calisma 1
-                    // calisma 2
-                    // calisma 3
-                    // calisma 4 
                 ) {
                     RecipeListScreen()
                 }
